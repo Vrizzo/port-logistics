d10d30f3f3439f5a01d93ad01da2b85119e720fd
Vittorio Rizzo <vittorio.rizzo@lastminute.com>
2020-09-11 22:37:12 +0200

    a working printer

--- src/main/java/com/vrizzo/port/printer/PortPrinter.java
+++ src/main/java/com/vrizzo/port/printer/PortPrinter.java
@@ -1,5 +1,11 @@
 package com.vrizzo.port.printer;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class PortPrinter
 {
   private final PrintablePortElement[] printablePortElements;
@@ -11,6 +17,40 @@ public class PortPrinter
 
   public String print()
   {
-    return "";
+    int infoMaxLenghto  = getInfoMaxLength();
+
+    final List<String> printableStringList = Stream.of(printablePortElements)
+        .map(PrintablePortElement::getPrintableInfo)
+        .map(s -> StringUtils.rightPad(s, infoMaxLenghto))
+        .collect(Collectors.toList());
+
+
+      
+    return "-^-^\n" + printPrintableString(printableStringList);
+  }
+
+  private String printPrintableString(List<String> printableStringList)
+  {
+    StringBuilder printableStringResult = new StringBuilder();
+    List<String> remainingPrintableStringList = new ArrayList<>();
+    for (String s : printableStringList)
+    {
+      final int lastCharIndex = s.length() -1;
+      if(lastCharIndex < 0)
+        return "";
+
+      printableStringResult.append(s.charAt(lastCharIndex));
+      remainingPrintableStringList.add(s.substring(0, lastCharIndex));
+    }
+    printableStringResult.append("\n").append(printPrintableString(remainingPrintableStringList));
+    return printableStringResult.toString();
+  }
+
+  private int getInfoMaxLength()
+  {
+    return Stream.of(printablePortElements)
+    .map(PrintablePortElement::getPrintableInfo)
+    .mapToInt(String::length)
+    .max().orElse(0);
   }
 }
--- src/test/java/com/vrizzo/port/printer/PortPrinterTest.java
+++ src/test/java/com/vrizzo/port/printer/PortPrinterTest.java
@@ -10,8 +10,24 @@ public class PortPrinterTest
 {
 
   @Test
-  public void print()
+  public void printEmpty()
   {
-    Assert.assertThat(new PortPrinter(() -> "").print(), is(""));
+    Assert.assertThat(new PortPrinter(() -> "").print(), is("-^-^\n"));
+  }
+
+  @Test
+  public void printOneElement()
+  {
+    Assert.assertThat(new PortPrinter(() -> "VXXXX").print(), is("-^-^\nX\nX\nX\nX\nV\n"));
+  }
+
+  @Test
+  public void printTwoElementEmpty()
+  {
+    final String print = new PortPrinter(() -> "VXXXX",
+                                         () -> "AXXXXX").print();
+    
+    Assert.assertThat(print, is("-^-^\n X\nXX\nXX\nXX\nXX\nVA\n"));
+    
   }
 }
