579990758ff0d29bff9554f822b2445d9f198dda
Vittorio Rizzo <vittorio.rizzo@lastminute.com>
2020-09-12 00:35:31 +0200

    testcase 2

--- src/test/java/com/vrizzo/port/ITPortTest.java
+++ src/test/java/com/vrizzo/port/ITPortTest.java
@@ -7,6 +7,7 @@ import static org.hamcrest.CoreMatchers.is;
 
 public class ITPortTest
 {
+  public static final Ship SHIP_4_CONTAINER = new Ship(4);
   String A_STANDARD_PORT_WITH_A_FULL_SHIP_AND_ONE_WAGON_READY = "-^-^\n"
       + " | |        \n"
       + " | |        \n"
@@ -14,6 +15,15 @@ public class ITPortTest
       + "X| |        \n"
       + "X| |        \n"
       + "X| |  X  D i\n"
+      + "VA_A---::%%%\n";
+
+  String A_STANDARD_PORT_WITH_A_FULL_SHIP = "-^-^\n"
+      + " | |        \n"
+      + " | |        \n"
+      + "X| |        \n"
+      + "X| |        \n"
+      + "X| |        \n"
+      + "X| |     D i\n"
       + "VA_A---::%%%\n";
 
   String A_STANDARD_PORT = "-^-^\n"
@@ -32,7 +42,7 @@ public class ITPortTest
     final Train train = new Train(3);
     train.loadContainer(1);
     Port port = new Port(train, new ContainerStorage(6), new PortInfoFormatter());
-    port.receiveShip(new Ship(4));
+    port.receiveShip(SHIP_4_CONTAINER);
     final String show = port.show();
 
     Assert.assertThat(show, is(A_STANDARD_PORT_WITH_A_FULL_SHIP_AND_ONE_WAGON_READY));
@@ -43,6 +53,13 @@ public class ITPortTest
   public void testCase1()
   {
     Assert.assertThat(port.show(), is(A_STANDARD_PORT));
+  }
+
+  @Test
+  public void testCase2()
+  {
+    port.receiveShip(SHIP_4_CONTAINER);
+    Assert.assertThat(port.show(), is(A_STANDARD_PORT_WITH_A_FULL_SHIP));
   }
 
   @Before
